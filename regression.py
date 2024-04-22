bfb697df1c68878a61da0d25c0e8d7f7d507d603
Pyveslefebvre <pyveslefebvre@gmail.com>
2024-04-22 16:18:42 +0200

    Random forest

--- regression.py
+++ regression.py
@@ -1,187 +1,192 @@
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.ensemble import RandomForestRegressor  # Import de RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import plot_tree
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from sklearn.tree import DecisionTreeRegressor  # Import de DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor
-from tensorflow.keras import Sequential     # Import du réseau de Neurone
+from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import StandardScaler, Normalizer
-from random import randint
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from random import randint
 
-
-"""Ouverture Data"""
+"""Data Loading"""
 
 df = pd.read_csv("datasets/samsung_dataset.csv", delimiter=";", on_bad_lines="warn")
-# Convertir la colonne de dates en un format temporel
 df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
 
-# Extraire des caractéristiques significatives de la date
+# Extracting significant features from the date
 df['Year'] = df['Date'].dt.year
 df['Month'] = df['Date'].dt.month
 df['Day'] = df['Date'].dt.day
 
-# Supprimer la colonne de dates originale si nécessaire
+# Dropping the original date column if necessary
 df = df.drop("Date", axis=1)
 
-# Modifier les virgule en point 
+# Replace commas with periods
 df = df.replace(',', '.', regex=True)
 
-# Enlever les ligne commprenant un zéro 
-df[df=='0']=np.nan
+# Removing rows containing zeros
+df[df=='0'] = np.nan
 df = df.dropna()
 
-X = df.drop("High",axis=1)
+X = df.drop("High", axis=1)
 y = df["High"]
 
-# Standardisation/Normalisation des valeur
-scale = StandardScaler()
-norm = Normalizer()  # Marche mieux avec la Normalisation
-
+# Normalizing the features
+norm = Normalizer()
 X = norm.fit_transform(X)
+
+# Storing column names for later use
+column_names = df.drop("High", axis=1).columns
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=21)
 
 """Random Forest"""
 
-# Division des données en ensemble d'apprentissage et ensemble de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=21)
-
-# Initialisation du modèle de la forêt aléatoire avec 100 arbres et une graine aléatoire fixée pour la reproductibilité.
-rf = RandomForestRegressor(n_estimators=100, max_depth = 3, random_state=1)  # Utilisation de RandomForestRegressor
-
-# Entraînement du modèle sur l'ensemble d'apprentissage
+# Initializing the Random Forest model with 100 trees and fixed random state for reproducibility
+rf = RandomForestRegressor(n_estimators=100, max_depth=30,max_features='sqrt',bootstrap=True,min_samples_leaf=1,
+                           min_samples_split=2, random_state=1)
 rf.fit(X_train, y_train)
 
-# Prédiction sur l'ensemble de test
 y_pred = rf.predict(X_test)
-
-# Calcul de l'erreur quadratique moyenne (MSE) du modèle en comparant les prédictions avec les valeurs réelles
 mse_rf = mean_squared_error(y_test, y_pred)
-
-# Affichage de l'erreur quadratique moyenne (MSE) du modèle
-print('Erreur quadratique moyenne (MSE) du modèle Random Forest:', mse_rf,"\n")
-
-# Calcul du coefficient de détermination R²
 r2_rf = r2_score(y_test, y_pred)
 
-# Affichage du coefficient de détermination R²
-print('Coefficient de détermination (R²) du modèle Random Forest:', r2_rf,"\n")
-
-# Sélection d'un seul arbre pour RandomForestRegressor et visualisation
-estimator_rf = rf.estimators_[0]
-plt.figure(figsize=(20,10))
-plot_tree(estimator_rf, filled=True, feature_names=X_train.columns, max_depth=3, fontsize=10)
-plt.title('Visualisation d\'un arbre de la forêt aléatoire')  # Ajouter un titre
+# Plot prediction error
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, y_pred, label='Predictions')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], '--', color='red', label='Perfect Prediction')
+plt.title('Prediction Error')
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
+plt.legend()
 plt.show()
+
+"""
+# Définir la grille de recherche des hyperparamètres
+param_grid = {
+    'n_estimators': [50,75,100,125,150,175,200,225,250,275,300],  # Nombre d'arbres dans la forêt
+    'max_features': ['auto', 'sqrt'],  # Nombre de caractéristiques à considérer à chaque division
+    'max_depth': [None, 10,15,20,25,30],  # Profondeur maximale de l'arbre
+    'min_samples_split': [2, 5, 10],  # Nombre minimum d'échantillons requis pour diviser un nœud
+    'min_samples_leaf': [1, 2, 4],  # Nombre minimum d'échantillons requis à chaque nœud feuille
+    'bootstrap': [True, False]  # Utiliser Bootstrap pour construire les arbres
+}
+
+# Initialiser le modèle Random Forest
+rf = RandomForestRegressor()
+
+# Initialiser RandomizedSearchCV
+rf_random = RandomizedSearchCV(estimator=rf, param_distributions=param_grid, n_iter=100, cv=3, verbose=2, random_state=42, n_jobs=-1)
+
+# Exécuter RandomizedSearchCV pour trouver les meilleurs paramètres
+rf_random.fit(X_train, y_train)
+
+# Afficher les meilleurs paramètres
+print(rf_random.best_params_)
+
+# Utiliser les meilleurs paramètres pour entraîner un nouveau modèle
+best_rf_model = rf_random.best_estimator_
+best_rf_model.fit(X_train, y_train)
+
+# Prédire avec le modèle optimisé
+y_pred = best_rf_model.predict(X_test)
+
+# Calculer les métriques de performance
+mse_rf = mean_squared_error(y_test, y_pred)
+r2_rf = r2_score(y_test, y_pred)
+
+# Afficher les MSE et R2
+print("MSE:", mse_rf)
+print("R-squared:", r2_rf)
+
+# Afficher les prédictions
+print("Prédictions:", y_pred)
+
+# Afficher les meilleurs paramètres
+print("Meilleurs paramètres:", rf_random.best_params_)
+"""
 
 """Decision Tree"""
 
-# Initialisation du modèle d'arbre de décision de régression
-tree = DecisionTreeRegressor(random_state=1)  # Utilisation de DecisionTreeRegressor avec une profondeur maximale de 3
-
-# Entraînement du modèle sur l'ensemble d'apprentissage
+tree = DecisionTreeRegressor(random_state=1, max_depth=3)
 tree.fit(X_train, y_train)
-
-# Prédiction sur l'ensemble de test
 y_pred_tree = tree.predict(X_test)
 
-# Calcul de l'erreur quadratique moyenne (MSE) et du coefficient de détermination (R²) pour le modèle Gradient Boosting
 mse_tree = mean_squared_error(y_test, y_pred_tree)
-rmse_tree = np.sqrt(mse_tree)
 r2_tree = r2_score(y_test, y_pred_tree)
 
-# Visualisation de l'arbre de régression DecisionTreeRegressor
 plt.figure(figsize=(20, 10))
-plot_tree(tree, filled=True, feature_names=X_train.columns, max_depth=3, fontsize=10)
-plt.title('Visualisation de l\'arbre de décision de régression')  # Ajouter un titre différent
+plot_tree(tree, filled=True, feature_names=column_names, max_depth=3, fontsize=10)
+plt.title('Decision Tree Regression Visualization')
 plt.show()
 
-# Initialisation du GridSearch pour fixé les meilleurs hyperparametre
 param_grid = {
     "max_depth": [None, 3],
     "max_features": [randint(1, 5), randint(5, 10), randint(10, 15)],
-    "criterion": ["squared_error", "absolute_error"],
-
+    "criterion": ["mse", "friedman_mse"],
 }
 
 tree_RS = RandomizedSearchCV(tree, param_grid, cv=20)
-
 tree_RS.fit(X_train, y_train)
 y_pred_tree_RS = tree_RS.predict(X_test)
 
 mse_tree_RS = mean_squared_error(y_test, y_pred_tree_RS)
-rmse_tree_RS = np.sqrt(mse_tree_RS)
 r2_tree_RS = r2_score(y_test, y_pred_tree_RS)
 
-print('L\'Erreur quadratique with DecisionTree : ', rmse_tree)  # Doit être faible
-print('L\'Erreur quadratique with optimized hyperparametre : ', rmse_tree_RS)  # Doit être faible
-print('Score r2 with DecisionTree : ', r2_tree)  # Doit tendre vers 0 (0<r2<1)
-print('Score r2 with optimized hyperparametre : ', r2_tree_RS)  # Doit tendre vers 0 (0<r2<1)
-
-# Affichage du graphique
-plt.show()
 
 """Gradient Boosting"""
 
-# Initialisation du modèle GradientBoostingRegressor
 gbr = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=1)
-
-# Entraînement du modèle sur l'ensemble d'apprentissage
 gbr.fit(X_train, y_train)
-
-# Prédiction sur l'ensemble de test
 y_pred_gbr = gbr.predict(X_test)
 
-# Calcul de l'erreur quadratique moyenne (MSE) et du coefficient de détermination (R²) pour le modèle Gradient Boosting
 mse_gbr = mean_squared_error(y_test, y_pred_gbr)
 r2_gbr = r2_score(y_test, y_pred_gbr)
 
-# Affichage des résultats
-print('Erreur quadratique moyenne (MSE) du modèle Gradient Boosting:', mse_gbr,"\n")
-print('Coefficient de détermination (R²) du modèle Gradient Boosting:', r2_gbr,"\n")
-
-# Tracé de l'importance des caractéristiques
 plt.figure(figsize=(10, 8))
 feature_importance = gbr.feature_importances_
 sorted_idx = np.argsort(feature_importance)
 pos = np.arange(sorted_idx.shape[0]) + .5
 plt.barh(pos, feature_importance[sorted_idx], align='center')
-plt.yticks(pos, np.array(X.columns)[sorted_idx])
-plt.title('Importance des caractéristiques pour le modèle Gradient Boosting')
+plt.yticks(pos, np.array(column_names)[sorted_idx])
+plt.title('Feature Importance for Gradient Boosting Model')
 plt.show()
 
-"""Réseau de neurone"""
+"""Neural Network"""
 
-
-# Normalisation des caractéristiques pour améliorer la performance du réseau de neurones
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Création du modèle de réseau de neurones
 model = Sequential()
-model.add(Dense(32, input_dim=X_train_scaled.shape[1], activation='relu'))  # Couche d'entrée
-model.add(Dense(16, activation='relu'))  # Couche cachée
-model.add(Dense(1, activation='linear'))  # Couche de sortie
+model.add(Dense(32, input_dim=X_train_scaled.shape[1], activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='linear'))
 
-# Compilation du modèle
 model.compile(loss='mean_squared_error', optimizer='adagrad')
 
-# Entraînement du modèle
 history = model.fit(X_train_scaled, y_train, validation_split=0.2, epochs=100, batch_size=10, verbose=0)
-
-# Évaluation du modèle
 loss = model.evaluate(X_test_scaled, y_test)
 
-
-# Tracer la courbe de performance
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='validation')
-plt.title('Modèle Loss')
+plt.title('Model Loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend()
 plt.show()
+
+"""Affichage"""
+print("Random forest RMSE:", np.sqrt(mse_rf))
+print("Random forest R²:", r2_rf)
+print('Decision Tree RMSE: ', np.sqrt(mse_tree))
+print('Decision Tree RMSE Optimized Hyperparameters: ', np.sqrt(mse_tree_RS))
+print('Decision Tree R²: ', r2_tree)
+print('Decision Tree R² Optimized Hyperparameters: ', r2_tree_RS)
+print('Gradient Boosting RMSE:', np.sqrt(mse_gbr))
+print('Gradient Boosting R²:', r2_gbr)
